654cf939735ae484bc8d729a84e3b3ca5e51b370
Chainarong C <110330650+Chainchigit@users.noreply.github.com>
2022-09-16 09:40:24 +0700

    Update Study Partition By Clause.sql

--- Study Partition By Clause.sql
+++ Study Partition By Clause.sql
@@ -1,3 +1,4 @@
+#PARTITION BY Clause in Action
 SELECT DISTINCT
        flight_number,
        aircraft_model,
@@ -9,7 +10,6 @@ FROM paris_london_flights
 ORDER BY flight_number, aircraft_model;
 
 ----------------------------------#1------------------------------------
-
 WITH year_month_data AS (
   SELECT DISTINCT
        EXTRACT(YEAR FROM scheduled_departure) AS year,
@@ -29,7 +29,6 @@ SELECT  year,
 FROM year_month_data;
 
 --------------------------------------#2---------------------------------
-
 WITH paris_london_delays AS (
   SELECT DISTINCT
        aircraft_model,
@@ -55,3 +54,57 @@ FROM paris_london_delays
 ORDER BY 1,2,3
 
 --------------------------------#3--------------------------------------------
+#Calculate the Moving Average
+SELECT
+  shop,
+  date,
+  revenue_amount,
+  AVG(revenue_amount) OVER (
+    PARTITION BY shop
+    ORDER BY date ASC
+    RANGE BETWEEN INTERVAL '1' DAY PRECEDING AND CURRENT ROW
+  ) AS moving_avg
+FROM revenue_per_shop;
+
+---------------------------------#4-------------------------------------------
+#Moving Average for Databases That Don't Support Using RANGE with Date/Time Data Types
+SELECT
+  shop,
+  date,
+  revenue_amount,
+  date - '2021_05_01' AS day_difference,
+  AVG(revenue_amount) OVER (
+    PARTITION BY shop
+    ORDER BY (date - '2021_05_01')
+    RANGE BETWEEN 1 PRECEDING AND CURRENT ROW
+  ) AS moving_avg
+FROM revenue_per_shop;
+
+---------------------------------#5-------------------------------------------
+#Find the Last Value Within a Range
+SELECT
+  shop,
+  date,
+  revenue_amount,
+  LAST_VALUE(revenue_amount) OVER (
+    PARTITION BY shop
+    ORDER BY date
+    RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+  ) AS last_value
+FROM revenue_per_shop;
+
+--------------------------------#6---------------------------------------------
+#Find the Number of Items Within a Range
+SELECT
+  shop,
+  date,
+  revenue_amount,
+  COUNT(*) OVER (
+    ORDER BY revenue_amount ASC
+    RANGE BETWEEN 1000 PRECEDING AND 1000 FOLLOWING
+  ) AS number_of_days
+FROM revenue_per_shop;
+
+-------------------------------#7----------------------------------------------
+
+
